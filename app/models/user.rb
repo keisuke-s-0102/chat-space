128a7c94a9349a5ff75db10cd5f5829b926aea22
keisuke-s-0102 <69072620+keisuke-s-0102@users.noreply.github.com>
2020-08-23 17:51:48 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          validates :name, presence: true, uniqueness: {case_sensitive: true}
-  
+　  　　　# Uniqueness validator will no longer enforce case sensitive comparison in Rails 6.1."という警告への対処
   has_many :group_users
   has_many :groups, through: :group_users
 
