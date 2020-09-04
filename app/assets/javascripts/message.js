976116efbef5f93048379a3efbc4bf9f0ef94771
keisuke-s-0102 <69072620+keisuke-s-0102@users.noreply.github.com>
2020-09-04 22:55:34 +0900

    incremental search_01

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -57,7 +57,7 @@ $(function(){
       $('.messages').append(html);      
       $('form')[0].reset();
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-      $('.Form__submit').prop("disabled", false);
+      $('.submit').prop("disabled", false);
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
